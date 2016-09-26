cf0c10945dd6b51ca30bce3ce426d869ccb63c7c
Badaueba <alan.develop@outlook.com>
2016-09-26 00:59:20 -0300

    main route '/' bug fix

--- server.js
+++ server.js
@@ -24,7 +24,7 @@ app.use( (req, res, next) => {
     res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With, Content-Type, Authorization, X-Access-Token, Origin, Accept');
     next();
 });
-app.get("*", function (req, res) {
+app.get("/", function (req, res) {
     res.send( path.join( __dirname + "/public/index.html"));
 });
 
